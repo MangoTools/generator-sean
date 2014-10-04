83cac55b1465d32e2babbd90d101669d12e3e7f7
adidoo <adrien@mango.tools>
2014-10-04 19:03:59 +0200

    updated server.controller of crud

--- crud-module/templates/express-module/_.server.controller.js
+++ crud-module/templates/express-module/_.server.controller.js
@@ -1,105 +1,110 @@
 'use strict';
 
 /**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-	errorHandler = require('./errors'),
-	<%= classifiedSingularName %> = mongoose.model('<%= classifiedSingularName %>'),
-	_ = require('lodash');
+* Module dependencies.
+*/
+
+var _ = require('lodash'),
+  db = require('../../config/sequelize'),
+  errorHandler = require('./errors');
 
 /**
- * Create a <%= humanizedSingularName %>
- */
+* Create a <%= humanizedSingularName %>
+*/
 exports.create = function(req, res) {
-	var <%= camelizedSingularName %> = new <%= classifiedSingularName %>(req.body);
-	<%= camelizedSingularName %>.user = req.user;
+  var <%= camelizedSingularName %> = req.body;
 
-	<%= camelizedSingularName %>.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(<%= camelizedSingularName %>);
-		}
-	});
+  <%= camelizedSingularName %>.UserId = req.user.id;
+
+  db.<%= classifiedSingularName %>.create(req.body).done(function(err, .<%= camelizedSingularName %>){
+    if(err){
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+    return res.jsonp(<%= camelizedSingularName %>);
+  });
 };
 
 /**
- * Show the current <%= humanizedSingularName %>
- */
+* Show the current <%= humanizedSingularName %>
+*/
 exports.read = function(req, res) {
-	res.jsonp(req.<%= camelizedSingularName %>);
+  res.jsonp(req.<%= camelizedSingularName %>);
 };
 
 /**
- * Update a <%= humanizedSingularName %>
- */
+* Update a <%= humanizedSingularName %>
+*/
 exports.update = function(req, res) {
-	var <%= camelizedSingularName %> = req.<%= camelizedSingularName %> ;
 
-	<%= camelizedSingularName %> = _.extend(<%= camelizedSingularName %> , req.body);
+  var <%= camelizedSingularName %> = req.<%= camelizedSingularName %>;
 
-	<%= camelizedSingularName %>.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(<%= camelizedSingularName %>);
-		}
-	});
+  <%= camelizedSingularName %> = _.extend(<%= camelizedSingularName %>, req.body);
+
+  <%= camelizedSingularName %>.save().done(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(<%= camelizedSingularName %>);
+    }
+  });
 };
 
 /**
- * Delete an <%= humanizedSingularName %>
- */
+* Delete an <%= humanizedSingularName %>
+*/
 exports.delete = function(req, res) {
-	var <%= camelizedSingularName %> = req.<%= camelizedSingularName %> ;
+  var <%= camelizedSingularName %> = req.<%= camelizedSingularName %>;
 
-	<%= camelizedSingularName %>.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(<%= camelizedSingularName %>);
-		}
-	});
+  <%= camelizedSingularName %>.destroy().done(function(err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(<%= camelizedSingularName %>);
+    }
+  });
 };
 
 /**
- * List of <%= humanizedPluralName %>
- */
-exports.list = function(req, res) { <%= classifiedSingularName %>.find().sort('-created').populate('user', 'displayName').exec(function(err, <%= camelizedPluralName %>) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(<%= camelizedPluralName %>);
-		}
-	});
+* List of <%= humanizedPluralName %>
+*/
+exports.list = function(req, res) {
+  db.<%= classifiedSingularName %>.findAll({ include: [{ model: db.User, attributes: ['displayName'] }], order: 'created' }).done(function(err, <%= camelizedPluralName %>) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.jsonp(<%= camelizedPluralName %>);
+    }
+  });
 };
 
 /**
- * <%= humanizedSingularName %> middleware
- */
-exports.<%= camelizedSingularName %>ByID = function(req, res, next, id) { <%= classifiedSingularName %>.findById(id).populate('user', 'displayName').exec(function(err, <%= camelizedSingularName %>) {
-		if (err) return next(err);
-		if (! <%= camelizedSingularName %>) return next(new Error('Failed to load <%= humanizedSingularName %> ' + id));
-		req.<%= camelizedSingularName %> = <%= camelizedSingularName %> ;
-		next();
-	});
+* <%= humanizedSingularName %> middleware
+*/
+exports.<%= camelizedSingularName %>ByID = function(req, res, next, id) {
+  db.<%= classifiedSingularName %>.find({where: { id: id }, include: [ { model: db.User, attributes: ['displayName'] } ] }).done(function(err, <%= camelizedSingularName %>) {
+    if (err) return next(err);
+    if (!<%= camelizedSingularName %>) return next(new Error('Failed to load <%= humanizedSingularName %> ' + id));
+    req..<%= camelizedSingularName %> = <%= camelizedSingularName %>;
+    next();
+  });
 };
 
 /**
- * <%= humanizedSingularName %> authorization middleware
- */
+* <%= humanizedSingularName %> authorization middleware
+*/
 exports.hasAuthorization = function(req, res, next) {
-	if (req.<%= camelizedSingularName %>.user.id !== req.user.id) {
-		return res.status(403).send('User is not authorized');
-	}
-	next();
+  if (req.<%= camelizedSingularName %>.user.id !== req.user.id) {
+    return res.status(403).send({
+      message: 'User is not authorized'
+    });
+  }
+  next();
 };
