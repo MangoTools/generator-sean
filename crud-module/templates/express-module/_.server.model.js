587b5d696c47e9a9f3f1b59f51edf50eb0ab7763
adidoo <adrien@mango.tools>
2014-10-05 12:34:06 +0200

    updated server.model of crud

--- crud-module/templates/express-module/_.server.model.js
+++ crud-module/templates/express-module/_.server.model.js
@@ -27,3 +27,28 @@ var <%= classifiedSingularName %>Schema = new Schema({
 });
 
 mongoose.model('<%= classifiedSingularName %>', <%= classifiedSingularName %>Schema);
+
+'use strict';
+
+/**
+ * <%= humanizedSingularName %> Schema
+ */
+module.exports = function(sequelize, DataTypes) {
+  var <%= classifiedSingularName %> = sequelize.define('<%= classifiedSingularName %>', {
+      name: {
+        type: DataTypes.STRING,
+        defaultValue: '',
+        allowNull: false
+      },
+      created: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      }
+    },
+    {
+      associate: function(models){
+        <%= classifiedSingularName %>.belongsTo(models.User);
+      }
+    });
+  return <%= classifiedSingularName %>;
+};
