f961f6ce11f08979fb0ea0b7fcb8333f4f7798ce
Jean-Baptiste Blanc <jbblanc@gmail.com>
2014-10-07 10:56:33 +0100

    api fix

--- crud-module/templates/angular-module/services/_.client.service.js
+++ crud-module/templates/angular-module/services/_.client.service.js
@@ -3,7 +3,7 @@
 //<%= humanizedPluralName %> service used to communicate <%= humanizedPluralName %> REST endpoints
 angular.module('<%= slugifiedPluralName %>').factory('<%= classifiedPluralName %>', ['$resource',
 	function($resource) {
-		return $resource('api/<%= slugifiedPluralName %>/:<%= camelizedSingularName %>Id', { <%= camelizedSingularName %>Id: '@_id'
+		return $resource('api/<%= slugifiedPluralName %>/:<%= camelizedSingularName %>Id', { <%= camelizedSingularName %>Id: '@id'
 		}, {
 			  update: {method: 'PUT'}
 		});
