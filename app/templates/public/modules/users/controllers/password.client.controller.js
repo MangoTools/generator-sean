f0dc21a07a6e9e96867d6024bae2a9395b4f5abe
adidoo <adrien@mango.tools>
2014-10-07 18:57:04 +0200

    password error by toaster

--- app/templates/public/modules/users/controllers/password.client.controller.js
+++ app/templates/public/modules/users/controllers/password.client.controller.js
@@ -14,18 +14,17 @@ angular.module('users').controller('PasswordController', ['$scope', '$stateParam
 			$http.post('/auth/forgot', $scope.credentials).success(function(response) {
 				// Show user success message and clear form
 				$scope.credentials = null;
-				$scope.success = response.message;
+				Message.success(response.message);
 
 			}).error(function(response) {
 				// Show user error message and clear form
 				$scope.credentials = null;
-				$scope.error = response.message;
+				Message.error(response.message);
 			});
 		};
 
 		// Change user password
 		$scope.resetUserPassword = function() {
-			$scope.success = $scope.error = null;
 
 			$http.post('/auth/reset/' + $stateParams.token, $scope.passwordDetails).success(function(response) {
 				// If successful show success message and clear form
@@ -37,7 +36,7 @@ angular.module('users').controller('PasswordController', ['$scope', '$stateParam
 				// And redirect to the index page
 				$location.path('/password/reset/success');
 			}).error(function(response) {
-				$scope.error = response.message;
+                Message.error(response.message);
 			});
 		};
 	}
